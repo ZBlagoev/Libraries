aae659dbd4de71d03f689b5475009b5e605f926c
ZBlagoev <zhelyazkoblagoev@gmail.com>
2020-12-18 11:14:51 +0200

    UsefullStuff - Matrixes - added methods of creating and reading matrixes + test program

---
+++ UsefullStuffLibrary/UsefullStuffLibrary/Matrixes.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace UsefullStuffLibrary
+{
+    public class Matrixes
+    {
+
+        // method for making square matrix where numbers are powers of X
+        public static int[,] MakeSqrMatrixPow(int n, int pow)
+        {
+            int[,] matrix = new int[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                int x = i;
+
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i, j] = (int)Math.Pow(pow, x);
+
+                    x++;
+                }
+            }
+
+            return matrix;
+        }
+
+        // method for making square matrix basic
+        public static int[,] MakeSqrMatrix(int n)
+        {
+            int[,] matrix = new int[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i, j] = 1;
+                }
+            }
+
+            return matrix;
+        }
+
+        // method for making rectangle matrix basic
+        public static int[,] MakeRecMatrix(int height, int length)
+        {
+            int[,] matrix = new int[height, length];
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < length; j++)
+                {
+                    matrix[i, j] = 1;
+                }
+            }
+
+            return matrix;
+        }
+
+        // method for printing matrix
+        public static void PrintMatrix(int[,] input)
+        {
+            for (int i = 0; i < input.GetLength(0); i++)
+            {
+                for (int j = 0; j < input.GetLength(1); j++)
+                {
+                    Console.Write($"{input[i, j]}\t");
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+        }
+
+
+
+
+    }
+}
---
+++ UsefullStuffLibrary/test_Project/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UsefullStuffLibrary;
+
+namespace test_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            
+            //int[,] leshta = Matrixes.MakeSqrMatrix(n);
+
+            int[,] leshta = Matrixes.MakeRecMatrix(n, m);
+
+            Matrixes.PrintMatrix(leshta);
+
+            */
+            int[,] matrix = new int[1, 5];
+
+            for (int i = 0; i < 1; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    matrix[i, j] = 1;
+                }
+            }
+
+            Console.WriteLine(matrix[0,4]);
+        }
+    }
+}
